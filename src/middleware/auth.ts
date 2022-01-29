434a25df6ab3ecf1eeda35f1ff70fad7c74d86f8
udden89 <udd.marcus@gmail.com>
2022-01-29 17:22:37 +0100

    Update auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,31 +1,31 @@
-import { NextFunction, Request, Response } from 'express';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import User from '../db/schemas/userSchema';
+import { NextFunction, Request, Response } from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import User from '../db/schemas/userSchema'
 
 const auth = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		//Får vårt token i req.header
-		const token = req.cookies.loggedIn;
+		const token = req.cookies.loggedIn
 		// req.header('Authorization')?.replace('Bearer ', '');
 		const decoded = jwt.verify(
 			token as string,
 			process.env.TOKEN_KEY as string
-		) as JwtPayload;
+		) as JwtPayload
 		const user = await User.findOne({
 			_id: decoded._id,
 			// 'tokens.token': token,
-		});
+		})
 
 		if (!user) {
-			throw new Error();
+			throw new Error()
 		}
 
-		req.body.token = token;
-		req.body.user = user;
+		req.body.token = token
+		req.body.user = user
 	} catch (error) {
-		res.status(401).send({ error: 'Please authenticate.' });
+		return res.status(401).send({ error: 'Please authenticate.' })
 	}
-	next();
-};
+	next()
+}
 
-export default auth;
+export default auth
